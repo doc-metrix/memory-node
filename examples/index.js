2a9e33114073a7783795cc54b0f799e7be1311a4
kgryte <kgryte@gmail.com>
2014-08-20 15:56:12 -0700

    [UPDATE] stubbed out interface. [TESTS] added tests.

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,78 @@
-var lib = require( './../lib' );
+/**
+*
+*	DEMO
+*
+*
+*	DESCRIPTION:
+*		- Demo for using the metrics API.
+*
+*
+*	NOTES:
+*		[1]
+*
+*
+*	TODO:
+*		[1] 
+*
+*
+*	LICENSE:
+*		MIT
+*
+*	Copyright (c) 2014. Athan Reines.
+*
+*
+*	AUTHOR:
+*		Athan Reines. athan@nodeprime.com. 2014.
+*
+*/
 
-// TODO: flesh this out more.
+(function() {
+	'use strict';
+
+	// MODULES //
+
+	var metrics = require( './../lib' );
+
+
+	// SCRIPT //
+
+	// Get the list of metrics:
+	console.log( metrics.list() );
+
+	/**
+	* Returns:
+	*	[
+	*		'metric0',
+	*		'metric1',
+	*		...
+	*		'metricN'
+	*	]
+	*/
+
+	// Check if a metric exists:
+	console.log( metrics.exists( 'mem.utilization' ) );
+
+	/**
+	* Returns:
+	*	true
+	*/
+
+	console.log( metrics.exists( 'cpu.utilization' ) );
+
+	/**
+	* Returns:
+	*	false
+	*/
+
+	// Get a metric specification:
+	console.log( metrics.get( 'mem.utilization' ) );
+
+	/**
+	* Returns:
+	*	[object]
+	*/
+
+})();
+
+
+
--- test/test.js
+++ test/test.js
@@ -4,8 +4,11 @@
 var // Expectation library:
 	chai = require( 'chai' ),
 
+	// Metrics specification:
+	METRICS = require( './../specs/memory.json' ),
+
 	// Module to be tested:
-	lib = require( './../lib' );
+	metrics = require( './../lib' );
 
 
 // VARIABLES //
@@ -19,10 +22,111 @@ var expect = chai.expect,
 describe( 'doc-metrix-memory', function tests() {
 	'use strict';
 
-	it( 'should export a function', function test() {
-		expect( lib ).to.be.a( 'function' );
+	// SETUP //
+
+	var NAMES = Object.keys( METRICS );
+
+
+	// TESTS //
+
+	it( 'should export an object', function test() {
+		expect( metrics ).to.be.an( 'object' );
 	});
 
-	it( 'should do something' );
+	describe( 'list', function tests() {
+
+		it( 'should provide a method to list all documented metrics', function test() {
+			expect( metrics.list ).to.be.a( 'function' );
+		});
+
+		it( 'should list all documented metrics', function test() {
+			assert.deepEqual( metrics.list(), NAMES );
+		});
+
+	});
+
+	describe( 'exists', function tests() {
+
+		it( 'should provide a method to check the existence of a metric specification', function test() {
+			expect( metrics.exists ).to.be.a( 'function' );
+		});
+
+		it( 'should not allow a non-string metric name', function test() {
+			var values = [
+					5,
+					[],
+					{},
+					true,
+					null,
+					undefined,
+					NaN,
+					function(){}
+				];
+
+			for ( var i = 0; i < values.length; i++ ) {
+				expect( badValue( values[i] ) ).to.throw( Error );
+			}
+
+			function badValue( value ) {
+				return function() {
+					metrics.exists( value );
+				};
+			}
+		});
+
+		it( 'should return false for a metric which does not have a specification', function test() {
+			assert.notOk( metrics.exists( 'cpu.utilization' ) );
+		});
+
+		it( 'should return true for a metric which has a specification', function test() {
+			assert.ok( metrics.exists( NAMES[0] ), true );
+		});
+
+	});
+
+	describe( 'get', function tests() {
+
+		it( 'should provide a method to get a metric specification', function test() {
+			expect( metrics.get ).to.be.a( 'function' );
+		});
+
+		it( 'should not allow a non-string metric name', function test() {
+			var values = [
+					5,
+					[],
+					{},
+					true,
+					null,
+					undefined,
+					NaN,
+					function(){}
+				];
+
+			for ( var i = 0; i < values.length; i++ ) {
+				expect( badValue( values[i] ) ).to.throw( Error );
+			}
+
+			function badValue( value ) {
+				return function() {
+					metrics.get( value );
+				};
+			}
+		});
+
+		it( 'should return null for a metric which does not have a specification', function test() {
+			assert.isNull( metrics.get( 'cpu.utilization' ) );
+		});
+
+		it( 'should return a metric specification', function test() {
+			assert.deepEqual( metrics.get( NAMES[0] ), METRICS[ NAMES[0] ] );
+		});
+
+	});
+
+	describe( 'source', function tests() {
+
+		it( 'should do something' );
+
+	});
 
 });
